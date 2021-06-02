4954e5e3367e6cef9a2f7749f888efa1d0fb2642
Jos3s <karatedkgt17@gmail.com>
2021-06-02 14:34:32 -0300

    Adicionando icone

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,5 +1,7 @@
 import "./styles.css";
 
+import pokedex from "./../../assets/pokedex.svg";
+
 const types = [
 	"grass",
 	"steel",
@@ -23,17 +25,37 @@ const num = Math.floor(Math.random() * types.length + 1);
 
 export const Header = () => (
 	<div className="header-contend">
-		<h1 className={"h1-" + types[num]}>Pokédex</h1>
-		<p>
-			por{" "}
-			<a href="https://www.linkedin.com/in/jos3s/" alt="Linkedin">
-				<b>José Ulisses</b>
+		<div className="box">
+			<a
+				href="https://www.flaticon.com/br/autores/roundicons-freebies"
+				title="Roundicons Freebies"
+			>
+				<img src={pokedex} alt="Pokédex" id="pokedex" />
 			</a>
-			, em 2021 -
-			<a href="https://github.com/jos3s/reactpokedex" alt="Github">
-				{" "}
-				<b>Github</b>
-			</a>
-		</p>
+			<div>
+				<h1 className={"h1-" + types[num]}>Pokédex</h1>
+				<p>
+					por{" "}
+					<a href="https://www.linkedin.com/in/jos3s/" alt="Linkedin">
+						<b>José Ulisses</b>
+					</a>{" "}
+					com{" "}
+					<a
+						href="https://www.flaticon.com/br/autores/roundicons-freebies"
+						title="Pokeapi"
+					>
+						<b>PokeAPI</b>
+					</a>
+					, em 2021 -
+					<a
+						href="https://github.com/jos3s/reactpokedex"
+						alt="Github"
+					>
+						{" "}
+						<b>Github</b>
+					</a>
+				</p>
+			</div>
+		</div>
 	</div>
 );
